dc267cb8ba395133d6815a29168e665a74365b99
Dr. Zeb <drzeb@machomath.com>
2020-03-23 13:51:04 -0500

    First Commit

---
+++ knowingjs/main.js
@@ -0,0 +1,20 @@
+//var x = 5;
+//console.log(x);
+//x = "This is a string";
+//console.log(x);
+var mainImg = document.querySelector(".img-container img");
+var rightButton = document.querySelector(".right-button");
+console.log(rightButton);
+rightButton.addEventListener("click", function(){
+  console.log(mainImg.src);
+  var mainImgSrc = mainImg.src;
+  var initMainImgSrc = mainImgSrc.slice(0, -5);
+  var midMainImgSrc = mainImgSrc.slice(-5, -4);
+  var endMainImgSrc = mainImgSrc.slice(-4);
+  midMainImgSrc = (midMainImgSrc - 0) + 1;
+  mainImgSrc = initMainImgSrc + midMainImgSrc + endMainImgSrc;
+  mainImg.src = mainImgSrc;
+  console.log(initMainImgSrc);
+  console.log(midMainImgSrc);
+  console.log(endMainImgSrc);
+});
