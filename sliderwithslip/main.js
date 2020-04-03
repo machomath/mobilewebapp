04220e12be3b4afe28ab46e86e412a3b2075216d
Dr. Zeb <drzeb@machomath.com>
2020-04-03 12:59:13 -0500

    Slider in progress

--- sliderwithslip/main.js
+++ sliderwithslip/main.js
@@ -1,10 +1,13 @@
 (function () {
   var numberOfImages = 5;
-  var autoFlipDirection = 1;
-  var mainImg = document.getElementsByClassName("my-main-img")[0];
+  var autoFlipDirection = -1;
+  var mainImgs = document.getElementsByClassName("my-main-img");
+  var onlyImgDiv = document.querySelector(".only-images");
   var buttons = document.querySelectorAll(".button");
   var autoFlip;
-  console.log(buttons);
+  var currentImgIndex = 1;
+  console.log(mainImgs[currentImgIndex]);
+
   buttons[1].addEventListener("click", function () {
     flipImg(1);
   });//right button
@@ -14,13 +17,17 @@
   });
 
   function flipImg(flipDirection){
-    var mainImgSrc = mainImg.src;
-    var initMainImgSrc = mainImgSrc.slice(0, -5);
-    var midMainImgSrc = mainImgSrc.slice(-5, -4);
-    var endMainImgSrc = mainImgSrc.slice(-4);
-    midMainImgSrc = ((midMainImgSrc - 0) + flipDirection + numberOfImages)%numberOfImages;
-    mainImgSrc = initMainImgSrc + midMainImgSrc + endMainImgSrc;
-    mainImg.src = mainImgSrc;
+    if(flipDirection != 0){
+      mainImgs[(currentImgIndex + 1*flipDirection + 3)%3].style.zIndex = "5";
+      mainImgs[currentImgIndex].style.left = (flipDirection == 1) ? "100%" : "-100%";//this line is sliding the image
+      mainImgs[currentImgIndex].addEventListener("transitionend", function resettingDeck() {
+        mainImgs[currentImgIndex].removeEventListener("transitionend", resettingDeck);
+        mainImgs[(currentImgIndex + 1*flipDirection + 3)%3].style.zIndex = "10";
+        mainImgs[currentImgIndex].style.zIndex = "0";
+        mainImgs[currentImgIndex].style.left = "0";
+      });
+      currentImgIndex = (currentImgIndex + 1*flipDirection + 3)%3;
+    }
   };
 
 
@@ -28,7 +35,7 @@
     flipImg(autoFlipDirection);
   }, 3000, autoFlipDirection);
 
-  mainImg.addEventListener("click", function () {
+  onlyImgDiv.addEventListener("click", function () {
     autoFlipDirection = (autoFlipDirection + 2)%3 -1;
   });
 
@@ -51,3 +58,11 @@
 
 
 })();
+
+var mainImgSrc = mainImg.src;
+var initMainImgSrc = mainImgSrc.slice(0, -5);
+var midMainImgSrc = mainImgSrc.slice(-5, -4);
+var endMainImgSrc = mainImgSrc.slice(-4);
+midMainImgSrc = ((midMainImgSrc - 0) + flipDirection + numberOfImages)%numberOfImages;
+mainImgSrc = initMainImgSrc + midMainImgSrc + endMainImgSrc;
+mainImg.src = mainImgSrc;
